c2c640553b71ed712a5912f4955c43382a141b19
Joshua Santiago <iamjosh004@gmail.com>
2021-08-26 11:20:42 +0800

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side js file') 
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From JavaScript'
+
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault() 
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
+response.json().then((data) =>{
+    if (data.error){
+        console.log(data.error);
+        messageOne.textContent = data.error
+    } else {
+        console.log(data.location);
+        console.log(data.forecast);
+        messageOne.textContent = data.location
+        messageTwo.textContent= data.forecast
+    }
+})
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require("request");
+
+const forecast = (lat, long, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=5f7a87819e10dc277c1f0f25bff2b231&query=" +
+    lat +
+    "," +
+    long +
+    "&units=f";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location. Try another search", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          `: It is current ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
